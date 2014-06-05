dc638a7d169856eed597960207e5e385695bd775
synboo <shu.shimbo@gmail.com>
2014-06-05 22:12:52 +0900

    Initial commit.

---
+++ SwiftFlickrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFlickrApp
+//
+//  Created by synboo on 6/5/14.
+//  Copyright (c) 2014 synboo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
